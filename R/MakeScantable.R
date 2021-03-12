35ed9faff05141ad216d5c3767903a5ba4ff04d5
Regina Lionheart <regina@lionhe.art>
2021-03-12 14:42:39 -0800

    Create MakeScantable mini function. Persistant errors at this step.

--- R/MakeScantable.R
+++ R/MakeScantable.R
@@ -1,15 +1,14 @@
+#' Create a filtered mini dataframe from a concatenated scanlist of MS2s.
+#'
+#' @param scan Single observation from a dataframe containing MS2 m/z and intensity spectra, separated by semicolons.
+#'
+#' @return scantable: A tiny dataframe, containing columns of mz and intensity.
+#' Intensity is scaled to 100 and filtered to drop all intensity values below 0.5.
+#' @export
+#'
+#' @examples
 MakeScantable <- function(scan) {
-  # Create a filtered MS2 scantable from a concatenated scanlist of MS2s.
-  #
-  # Args
-  #   scan: Single observation from a dataframe containing MS2 mz and intensity
-  #         spectra, separated by semicolons (;).
-  #
-  # Returns
-  #   scantable: Tiny dataframe, containing columns of mz and intensity.
-  #              Intensity is scaled to 100 and filtered to drop all intensity
-  #              values below 0.5.
-  #
+  requireNamespace("dplyr", quietly = TRUE)
   scantable <- read.table(text = as.character(scan),
                           col.names = c("mz", "intensity"), fill = TRUE) %>%
     mutate(mz = as.numeric(mz %>% str_replace(",", "")),
