5873006c17951bbdd45419f5adbe7acfb954f142
wkumler <wkumler@uw.edu>
2021-04-21 10:13:54 -0700

    Wrapping up CL1 review, code is functional now

--- R/AnnotateConfidenceLevel1.R
+++ R/AnnotateConfidenceLevel1.R
@@ -46,54 +46,63 @@ AnnotateConfidenceLevel1 <- function(experimental.values, theoretical.values, mz
   My.Fuzzy.Join <- theoretical.values %>%
     fuzzyjoin::difference_left_join(experimental.values, by = c("mz"), max_dist = mz.flexibility) %>%
     dplyr::rename_with(~gsub("\\.y", "_experimental", .x)) %>%
-    dplyr::rename_with(~gsub("\\.x", "_theoretical", .x)) %>% names()
-    dplyr::select(MassFeature, compound_experimental, compound_theoretical, mz_experimental, mz_theoretical, rt_sec_experimental, rt_sec_theoretical,
+    dplyr::rename_with(~gsub("\\.x", "_theoretical", .x)) %>%
+    # Lots of confusion about "MassFeature" vs "compound_experimental" - was broken
+    # because compound_experimental wasn't created anywhere, tried changing it to MassFeature with some success
+    dplyr::select(MassFeature, compound, mz_experimental, mz_theoretical, rt_experimental, rt_theoretical,
                   column_experimental, column_theoretical, z_experimental, z_theoretical, MS2_experimental, MS2_theoretical)  %>%
-    dplyr::arrange(compound_experimental)
+    dplyr::arrange(MassFeature)
 
   # Confidence Level 1 ----------------------------------------
   Confidence.Level.1 <- My.Fuzzy.Join %>%
     dplyr::filter(z_experimental == z_theoretical,
                   column_experimental == column_theoretical) %>%
     dplyr::mutate(mz_similarity_score1 = exp(-0.5 * (((mz_experimental - mz_theoretical) / mz.flexibility) ^ 2)), # This should be a function (calcSimScore?)
-                  rt_similarity_score1 = exp(-0.5 * (((rt_sec_experimental - rt_sec_theoretical) / rt.flexibility) ^ 2))) %>% # This is not how I expected the mz. and rt.flexibility to be used - needs more documentation
+                  rt_similarity_score1 = exp(-0.5 * (((rt_experimental - rt_theoretical) / rt.flexibility) ^ 2))) %>% # This is not how I expected the mz. and rt.flexibility to be used - needs more documentation
     dplyr::rowwise() %>%
-    dplyr::mutate(ppm_mass_error = ((abs(mz_experimental - mz_theoretical)) / mz_theoretical) * 10^6,
-                  MS2_cosine_similarity1 = ifelse(is.na(MS2_experimental) | is.na(MS2_theoretical),
-                                                 NA, MS2CosineSimilarity(MakeScantable(MS2_experimental), MakeScantable(MS2_theoretical))),
-                  # It might make sense to wrap MakeScanTable into MS2CosineSimilarity - the intuitive
-                  # representation of the code here is MS2CosineSimilarity(MS2_expected, MS2_observed)
-                  total_similarity_score = ifelse(is.na(MS2_cosine_similarity1),
+    dplyr::mutate(ppm_mass_error = ((abs(mz_experimental - mz_theoretical)) / mz_theoretical) * 10^6) %>%
+    dplyr::mutate(MS2_cosine_similarity1 = ifelse(is.na(MS2_experimental) | is.na(MS2_theoretical),
+                                                 NA, MS2CosineSimilarity(MakeScantable(MS2_experimental), MakeScantable(MS2_theoretical)))) %>%
+    # It might make sense to wrap MakeScanTable into MS2CosineSimilarity - the intuitive
+    # representation of the code here is MS2CosineSimilarity(MS2_expected, MS2_observed)
+    dplyr::mutate(total_similarity_score = ifelse(is.na(MS2_cosine_similarity1),
                                                   mean(c(mz_similarity_score1, rt_similarity_score1)), # Use "mean" to be intuitive
-                                                  mean(c(MS2_cosine_similarity1, mz_similarity_score1, rt_similarity_score1))))*100
+                                                  mean(c(MS2_cosine_similarity1, mz_similarity_score1, rt_similarity_score1)))*100)
 
 
   # Sanity check -------------------------------------------------------------
   # No fuzzy match (no mz within 0.02 daltons)
   No.Fuzzy.Match <- experimental.values %>%
-    dplyr::select(compound_experimental) %>%
-    # I don't think compound_experimental is always going to be a numeric? If it is, this should be either
-    # created internally or specified in the docs
-    dplyr::filter(compound_experimental %in% setdiff(1:nrow(.), My.Fuzzy.Join$compound_experimental)) %>%
+    dplyr::select(MassFeature) %>%
+    # Anti_join is much clearer here and avoids the 1:nrow(.) problem below
+    # anti_join(My.Fuzzy.Join, by="MassFeature") %>%
+    dplyr::filter(MassFeature %in% setdiff(1:nrow(.), My.Fuzzy.Join$MassFeature)) %>%
+    # Line above does not do what's expected because MassFeature is NOT numeric
+    # so checking if it's a number(?) doesn't work
     dplyr::pull()
 
   # Fuzzy match, but wrong z/column
+  # I have no idea what the below code does but I suspect it's not doing
+  # what we want it to. Again, this is probably because MassFeature isn't
+  # a numeric but instead a string (e.g. I137.0459R6.2)
   No.CL1.Match <- setdiff(1:nrow(experimental.values),
-                          sort(c(unique(Confidence.Level.1$compound_experimental),
+                          sort(c(unique(Confidence.Level.1$MassFeature),
                                  No.Fuzzy.Match)))
 
   # Have any compounds been lost? Check for a TRUE output
-  all.experimentals <- sort(c(No.Fuzzy.Match, No.CL1.Match, unique(Confidence.Level.1$compound_experimental)))
-  if(!length(all.experimentals) == length(experimental.values$compound_experimental)){
+  all.experimentals <- sort(c(No.Fuzzy.Match, No.CL1.Match, unique(Confidence.Level.1$MassFeature)))
+  # Creating an actual "stop" function here because returning a value internally
+  # isn't helpful to the actual user
+  if(!length(all.experimentals) == length(experimental.values$MassFeature)){
     stop("Lengths don't match") # Add details to this stop message - what are the two lengths? Why should they match?
   }
 
   # Make "no match" dataframes for comparison
   # These data frames should be more intuitively created by anti_join
   No.CL1.Match.df <- experimental.values %>%
-    dplyr::filter(compound_experimental %in% No.CL1.Match)
+    dplyr::filter(MassFeature %in% No.CL1.Match)
   No.Fuzzy.Match.df <- experimental.values %>%
-    dplyr::filter(compound_experimental %in% No.Fuzzy.Match)
+    dplyr::filter(MassFeature %in% No.Fuzzy.Match)
 
   # Let's land on MARS ------------------------------------------------------
   Mission.Accomplished <- Confidence.Level.1 %>%
@@ -102,12 +111,15 @@ AnnotateConfidenceLevel1 <- function(experimental.values, theoretical.values, mz
     dplyr::mutate(confidence_rank = ifelse(mz_similarity_score1 > 0.9 & rt_similarity_score1 > 0.75 & ppm_mass_error < 7, 1, NA),
                   confidence_source = ifelse(!is.na(confidence_rank), "Ingalls_Standards", NA)) %>%
     dplyr::mutate(mz_experimental = ifelse(is.na(mz_experimental) & !is.na(mz), mz, mz_experimental),
-                  rt_sec_experimental = ifelse(is.na(rt_sec_experimental) & !is.na(rt), rt, rt_sec_experimental),
+                  rt_experimental = ifelse(is.na(rt_experimental) & !is.na(rt), rt, rt_experimental),
            column_experimental = ifelse(is.na(column_experimental) & !is.na(column), column, column_experimental),
            z_experimental = ifelse(is.na(z_experimental) & !is.na(z), z, z_experimental),
            MS2_experimental = ifelse(is.na(MS2_experimental) & !is.na(MS2), MS2, MS2_experimental)) %>%
+    # This NA-nuking line of code is a little alarming - usually those NAs
+    # are valuable information to carry through the rest of the data. What's
+    # the thought process here?
     dplyr::select(MassFeature, everything(), -c("mz", "rt", "column", "z", "MS2"), ) %>%
-    dplyr::arrange(compound_experimental)
+    dplyr::arrange(MassFeature)
 
   return(Mission.Accomplished)
 }
