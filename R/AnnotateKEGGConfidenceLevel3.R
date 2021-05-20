af096ecf43009248158747aa3eed76890e5c3daa
Regina Lionheart <regina@lionhe.art>
2021-05-20 15:48:51 -0700

    First runthrough with KEGG CL3.

--- R/AnnotateKEGGConfidenceLevel3.R
+++ R/AnnotateKEGGConfidenceLevel3.R
@@ -22,58 +22,58 @@
 AnnotateKEGGConfidenceLevel3 <- function(Confidence.Level.3.MoNA, KEGG.Data, mz.flexibility, ppm.tolerance) {
 
   KEGG.Data <- KEGG.Data %>%
-    rename(Compound_KEGG = OtherCmpds)
+    dplyr::rename(Compound_KEGG = OtherCmpds)
 
   ToJoin <- Confidence.Level.3.MoNA %>%
-    rename(mz = mz_experimental) %>%
-    select(MassFeature, primary_key, mz, column_experimental, z_experimental) %>%
+    dplyr::rename(mz = mz_experimental) %>%
+    dplyr::select(MassFeature, primary_key, mz, column_experimental, z_experimental) %>%
     unique()
 
   # Isolate pos and neg theoretical data
   keggPos <- KEGG.Data %>%
-    select(Compound_KEGG, PosMZ) %>%
-    mutate(column_KEGG = "HILIC",
+    dplyr::select(Compound_KEGG, PosMZ) %>%
+    dplyr::mutate(column_KEGG = "HILIC",
            z = 1) %>%
-    rename(mz = PosMZ)
+    dplyr::rename(mz = PosMZ)
 
   keggNeg <- KEGG.Data %>%
-    select(Compound_KEGG, NegMZ) %>%
-    mutate(column_KEGG = "HILIC",
+    dplyr::select(Compound_KEGG, NegMZ) %>%
+    dplyr::mutate(column_KEGG = "HILIC",
            z = -1) %>%
-    rename(mz = NegMZ)
+    dplyr::rename(mz = NegMZ)
 
   # Combine to full theoretical dataset
   keggCompounds <- keggNeg %>%
     rbind(keggPos)
 
   # Fuzzyjoin datasets
-  My.Fuzzy.Join <- difference_inner_join(x = keggCompounds, y = ToJoin,
+  My.Fuzzy.Join <- fuzzyjoin::difference_inner_join(x = keggCompounds, y = ToJoin,
                                          by = "mz", max_dist = mz.flexibility, distance_col = NULL) %>%
-    rename(mz_KEGG = mz.x,
+    dplyr::rename(mz_KEGG = mz.x,
            mz_experimental = mz.y,
            z_KEGG = z) %>%
-    mutate(ppm = (abs(mz_KEGG - mz_experimental) / mz_KEGG *10^6),
+    dplyr::mutate(ppm = (abs(mz_KEGG - mz_experimental) / mz_KEGG *10^6),
            mz_similarity_scoreKEGG = exp(-0.5 * (((mz_experimental - mz_KEGG) / 0.02) ^ 2))) %>%
-    filter(ppm < ppm.tolerance,
+    dplyr::filter(ppm < ppm.tolerance,
            z_KEGG == z_experimental) %>%
     unique() %>%
-    rename(KEGGppm = ppm) %>%
-    arrange(primary_key)
+    dplyr::rename(KEGGppm = ppm) %>%
+    dplyr::arrange(primary_key)
 
   # Match KEGG names to IDs
-  matchedNames <- left_join(My.Fuzzy.Join, KEGG.Data) %>%
-    select(Compound_KEGG, Name) %>%
-    group_by(Compound_KEGG) %>%
-    summarise(KEGGMatchesNames = as.character(paste(Name,  collapse=" ")))
+  matchedNames <- dplyr::left_join(My.Fuzzy.Join, KEGG.Data) %>%
+    dplyr::select(Compound_KEGG, Name) %>%
+    dplyr::group_by(Compound_KEGG) %>%
+    dplyr::summarise(KEGGMatchesNames = as.character(paste(Name,  collapse=" ")))
 
   # Combine to full matched df
   final <- My.Fuzzy.Join %>%
-    left_join(matchedNames) %>%
-    select(MassFeature, primary_key, Compound_KEGG, mz_KEGG, z_KEGG, KEGGMatchesNames, KEGGppm, mz_similarity_scoreKEGG)
+    dplyr::left_join(matchedNames) %>%
+    dplyr::select(MassFeature, primary_key, Compound_KEGG, mz_KEGG, z_KEGG, KEGGMatchesNames, KEGGppm, mz_similarity_scoreKEGG)
   Confidence.Level.3.MoNA[Confidence.Level.3.MoNA == ""] <- NA
 
   Confidence.Level.3 <- final %>%
-    full_join(Confidence.Level.3.MoNA) %>%
+    dplyr::full_join(Confidence.Level.3.MoNA) %>%
     dplyr::arrange(primary_key) %>%
     unique() %>%
     dplyr::mutate(confidence_rank3 = ifelse(mz_similarity_scoreKEGG > 0.9, 3, NA)) %>%
@@ -85,8 +85,9 @@ AnnotateKEGGConfidenceLevel3 <- function(Confidence.Level.3.MoNA, KEGG.Data, mz.
     dplyr::select(primary_key, MassFeature, compound_theoretical, massbank_match2, massbank_match3, Compound_KEGG, KEGGMatchesNames,
                   mz_experimental:mz_massbank2, MH_mass_experimental, MH_mass_MoNA, mz_KEGG,
                   z_experimental, z_theoretical, z_massbank2, z_massbank3, z_KEGG, rt_sec_experimental:column_theoretical,
-                  MS2_experimental:MS2_massbank, ppm_mass_error1, massbank_ppm, KEGGppm, mz_similarity_score1, mz_similarity_score3,
-                  rt_similarity_score1:total_similarity_score1, everything(), -confidence_rank3)
+                  MS2_experimental:MS2_massbank, ppm_mass_error1, massbank_ppm, KEGGppm, mz_similarity_score1, mz_similarity_score3, mz_similarity_scoreKEGG,
+                  rt_similarity_score1:total_similarity_score1, everything(), -confidence_rank3) %>%
+    unique()
 
   return(Confidence.Level.3)
 
