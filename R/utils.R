3cd8f93260c0198e6978e7caf2b1f92c1f379ed6
Regina Lionheart <regina@lionhe.art>
2021-06-08 13:47:15 -0700

    Remove actionable todo items from list.

--- R/utils.R
+++ R/utils.R
@@ -57,6 +57,22 @@ checkContaminants <- function(experimental.df, common.contaminants, ppm.toleranc
   return(matchedKnownContaminants)
 }
 
+#' ConcatToScan
+#'
+#' @param concat.format MS2 data in "concatenated format": mz and intensity separated by comma,
+#' and separated from the next set of fragments by a semicolon. For example, "148.0, 100; 147.0, 50;", etc.
+#'
+#' @return scantable: The same MS2 data separated into a mini dataframe of mz and intensity.
+#' @export
+ConcatToScan <- function(concat.format) {
+  scantable <- cSplit(concat.format, "MS2", sep = ";") %>%
+    pivot_longer(cols = starts_with("MS2")) %>%
+    separate(value, sep = ",", into = c("mz", "intensity")) %>%
+    rename(fragment = name)
+
+  return(scantable)
+}
+
 #' Compare experimental mass features to scraped MoNA data, based on MS1 and MS2 information.
 #'
 #' @param MoNA.Mass Single MoNA mass, isolated from scraped MoNA df.
@@ -198,4 +214,50 @@ pasteWithoutNA <- function(..., sep = "; ") {
 #' @return
 ReplaceNA <- function(column) {
   gsub("NA; ", "", column)
+}
+
+#' Scale Concatenated MS2 data
+#'
+#' @param concatenated.scan MS2 data in concatenated format: paired mz and intensity values
+#' separated by a semicolon.
+#'
+#' @return scantable: MS2 data scaled so the highest intensity corresponds to 100, and all
+#' intensity values below 0.5 are thrown out.
+#' @export
+#'
+ScaleMS2 <- function(concatenated.scan) {
+  scantable <- read.table(text = as.character(concatenated.scan),
+                          col.names = c("mz", "intensity"), fill = TRUE) %>%
+    mutate(mz = as.numeric(mz %>% str_replace(",", "")),
+           intensity = as.numeric(intensity %>% str_replace(";", "")),
+           intensity = round(intensity / max(intensity) * 100, digits = 1)) %>%
+    filter(intensity > 0.5) %>%
+    arrange(desc(intensity)) %>%
+    unite("MS2", mz:intensity, sep = ", ") %>%
+    mutate(MS2 = paste(MS2, collapse = "; ")) %>%
+    unique()
+
+  return(scantable)
+}
+
+#' ScanToConcat
+#'
+#' @param scantable.format MS2 data in scantable format: a mini dataframe of mz and intensity.
+#'
+#' @return concatenated: the same MS2 data converted to a concatenated format of paured mz and intensity
+#' values separated by semicolons.
+#' @export
+ScanToConcat <- function(scantable.format) {
+  concatenated <- scantable.format %>%
+    unique() %>%
+    group_by(compound_name, filename) %>%
+    unite("MS2", mz:intensity, sep = ",") %>%
+    ungroup() %>%
+    group_by(compound_name, voltage) %>%
+    mutate(MS2 = paste(MS2, collapse = "; ")) %>%
+    select(-fragment) %>%
+    unique() %>%
+    as.data.frame()
+
+  return(concatenated)
 }
