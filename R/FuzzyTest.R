c616718db85522c58c1d4425aa66992aa44da952
Regina Lionheart <regina@lionhe.art>
2022-11-23 11:00:48 -0800

    Commit before expanding dataframes

--- R/FuzzyTest.R
+++ R/FuzzyTest.R
@@ -3,6 +3,23 @@ library(fuzzyjoin)
 
 ## Flexible fuzzy join test script
 
+## Functions
+GetFlex <- function(mz) { # Should molecule class and flex windows be arguments? Are they gonna change?
+  molecule.class <- c(100, 200, 500)
+  flex <- c(0.00025, 0.0005, 0.0012)
+  for (i in 1:length(molecule.class)) {
+    if (mz <= molecule.class[i]) {
+      return(c(flex[i]))
+    }
+  }
+  return(c(0.0025))
+}
+
+MzCloseEnough <- function(x, y) {
+  abs(x[, 1] - y) <= x[, 2]
+}
+
+## Load data
 experimental <- read.csv("example_data/Example_Experimental_Data.csv") %>%
   select(MassFeature, mz)
 
@@ -11,22 +28,13 @@ theoretical <- read.csv("example_data/Example_Theoretical_Data.csv") %>%
   unique() %>%
   drop_na()
 
-get_flex <- function(mz) {
-  intervals <- c(100, 200, 500)
-  flex <- c(0.00025, 0.0005, 0.0012)
-  for (i in 1:length(intervals)) {
-    if (mz <= intervals[i]) {
-      return(c(flex[i]))
-    }
-  }
-  return(c(0.0025))
-}
-experimental <- experimental %>% rowwise() %>% mutate(flex = get_flex(mz))
+## Sort experimental mzs into flex category
+experimental <- experimental %>%
+  rowwise() %>%
+  mutate(flex = GetFlex(mz))
 
-mz_close_enough <- function(x, y) {
-  abs(x[,1] - y) <= x[,2]
-}
-Current <- experimental %>%
+## Fuzzy join experimental and theoretical data according to flex category
+flex.join <- experimental %>%
   fuzzyjoin::fuzzy_join(theoretical, multi_by = list(x=c("mz", "flex"), y="mz"),
-                        multi_match_fun = mz_close_enough, mode = 'left')
+                        multi_match_fun = MzCloseEnough, mode = "left")
 
