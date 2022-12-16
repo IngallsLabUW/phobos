e97e104dd36fe8045381cfc7000406402067d723
Regina Lionheart <regina@lionhe.art>
2022-12-15 16:04:04 -0800

    Incorporate z and HILIC mix matching to avoid explosions.

--- R/Test_CompleteInitialRun.R
+++ R/Test_CompleteInitialRun.R
@@ -1,8 +1,10 @@
+library(pbapply)
 library(tidyverse)
 
 ### WKumler + RML Startup script
 
 # Notes -------------------------------------------------------------------
+# Prepping the data can be kind of a big step, see preparation for single.experimental
 # How do we want to handle voltage columns? Should we match on them?
 # MS2 Sim Score separate_rows produced some headaches.
 # Need to make an adjustment for "n consensus files", not 4:6/9:11 for the testing purposes. This could be a little softer...
@@ -10,6 +12,8 @@ library(tidyverse)
 # for two intensity clusters, but it's possible we'd have three, four...
 # Full run is taking 51 minutes.
 # Without the MS2 steps, the total process took 50 seconds.
+# ...and then we dropped the "remove any single MS2 files beforehand step" which bumped it back up to 7 minutes...
+# Amino Propanesulfonic acid: any relation to 3-Aminopropanesulfonate?
 
 # Outline -------------------------------------------------------------------
 # Create consensus MS2 data from four of the five MSMS runs of the Ingalls standards.
@@ -20,7 +24,7 @@ library(tidyverse)
 # Prepare all data -------------------------------------------------------------------
 ingalls.standards <- read.csv("https://raw.githubusercontent.com/IngallsLabUW/Ingalls_Standards/master/Ingalls_Lab_Standards.csv",
                                                     stringsAsFactors = FALSE, header = TRUE) %>%
-  select(compound_name = Compound_Name, mz, rt = RT_minute, column = Column, z) %>%
+  select(compound_name = Compound_Name, HILIC_Mix, mz, rt = RT_minute, column = Column, z) %>%
   mutate(rt = rt * 60) %>%
   distinct()
 
@@ -44,16 +48,26 @@ single.experimental <- read_csv("example_data/Ingalls_Lab_Standards_MSMS.csv") %
   ungroup() %>%
   arrange(desc(int)) %>%
   filter(int>1) %>%
-  group_by(voltage, compound_name, filename) %>%
-  summarize(MS2 = paste(mz, int, sep = ", ", collapse = "; ")) %>%
+  group_by(voltage, compound_name, filename) %>% #, z) %>%
+  summarise(MS2 = paste(mz, int, sep = ", ", collapse = "; ")) %>%
+  mutate(z = ifelse(str_detect(filename, "pos"), 1, -1)) %>%
+  mutate(HILIC_Mix = ifelse(str_detect(filename, "Mix1"), "Mix1", "Mix2")) %>%
   rowwise() %>%
-  summarize(MS2 = paste(voltage, MS2, sep = "V ", collapse = ": ")) %>%
+  mutate(MS2 = paste(voltage, MS2, sep = "V ", collapse = ": ")) %>%
   as.data.frame() %>%
-  left_join(ingalls.standards, by = "compound_name") %>%
-  mutate(voltage = sub("\\V.*", "", MS2)) %>% ## Currently just creating this voltage column
-  select(compound_name, voltage, mz, rt, column, z, MS2)
+  select(-filename) %>%
+  left_join(ingalls.standards, by = c("compound_name", "z", "HILIC_Mix")) %>%
+  select(compound_name, voltage, mz, rt, column, z, MS2) %>%
+  drop_na()
 
 # Functions ---------------------------------------------------------------
+# mz_i <- single.experimental$mz[25]
+# rt_i <- single.experimental$rt[25]
+# col_i <- single.experimental$column[25]
+# z_i <- single.experimental$z[25]
+# MS2str_i <- single.experimental$MS2[25]
+# ppm_error <- 10
+# theoretical_db <- consensus.theoretical
 
 CreatePotentialMatches_1 <- function(mz_i, rt_i, col_i, z_i,
                                      MS2str_i,
@@ -72,7 +86,7 @@ CreatePotentialMatches_1 <- function(mz_i, rt_i, col_i, z_i,
     mutate(MS1SimScore = CalculateMzSimScore_1(mz_exp = mz_i, mz_theo = mz, flex = 5)) %>%
     mutate(RT1SimScore = CalculateRTSimScore_1(rt_exp = rt_i, rt_theo = rt, flex = 30)) %>%
     mutate(MS2SimScore = as.numeric(ifelse(str_detect(MS2, ","),
-                               lapply(MS2, CalculateMS2SimScore_1, ms2_theo = MS2str_i, flex = 0.02), NA))) %>%
+                               pblapply(MS2, CalculateMS2SimScore_1, ms2_theo = MS2str_i, flex = 0.02), NA))) %>%
     mutate(TotalSimScore = CalculateTotalSimScore_1(MS1SimScore, RT1SimScore, MS2SimScore)) %>%
     select(compound_name, voltage, ends_with("SimScore")) ## Should this include voltage?
 
@@ -105,13 +119,18 @@ CalculateMzSimScore_1 <- function(mz_exp, mz_theo, flex) {
   return(similarity.score)
 } ## TODO The flex values are hardcoded in the Calctotalsimscore1 function
 
+# ms2_exp <- single.experimental$MS2[25]
+# ms2_theo <- consensus.theoretical$MS2[29]
+
 CalculateMS2SimScore_1 <- function(ms2_exp, ms2_theo, flex) {
 # Comparison will be between experimental and "consensus" spectra according to Horai et. al 2010
 # as justification for using spectra
   scan1 <- MakeScantable(ms2_exp)
   scan2 <- MakeScantable(ms2_theo)
 
-  if ((is.na(scan1) || is.na(scan2) || nrow(scan1) < 2 || nrow(scan2) < 2) == TRUE) {
+  if ((is.na(scan1) || is.na(scan2) || nrow(scan1) < 2 || nrow(scan2) < 2)) {
+  #if (is.na(scan1) || is.na(scan2) || all(dim(scan1) == c(1,1)) || all(dim(scan2) == c(1,1))) {
+
     return(NA)
 
   } else {
@@ -142,18 +161,17 @@ CalculateTotalSimScore_1 <- function(ms1_sim, rt_sim, ms2_sim) {
 
 
 ## Example: Produces dataframe of potential matches and all sim scores for a single row of experimental data.
-single.frame <- CreatePotentialMatches_1(mz_i = single.experimental$mz[1], rt_i = single.experimental$rt[1],
-                                col_i = single.experimental$column[1], z_i = single.experimental$z[1],
-                                MS2str_i = single.experimental$MS2[1],
+single.frame <- CreatePotentialMatches_1(mz_i = single.experimental$mz[25], rt_i = single.experimental$rt[25],
+                                col_i = single.experimental$column[25], z_i = single.experimental$z[25],
+                                MS2str_i = single.experimental$MS2[25],
                                 ppm_error = 100,
                                 theoretical_db = consensus.theoretical)
 
 
 ## Produces a dataframe with a nested column
 start.time <- Sys.time()
-
 AllOutput <- single.experimental %>%
-  slice(1:10) %>%
+  slice(1:30) %>%
   rowwise() %>%
   mutate(matches = list(CreatePotentialMatches_1(mz_i = mz, rt_i = rt, col_i = column, z_i = z,
                                                  MS2str_i = MS2,
@@ -167,9 +185,14 @@ AllOutput <- single.experimental %>%
       arrange(desc(med_sim_overall)) %>%
       slice(1) %>%
       pull(compound_name)
-  }))
+    }))
+
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 print(time.taken)
 
+
+### Fix the weird matching
+testing <- AllOutput %>%
+  filter(compound_name != top_choice)
 
